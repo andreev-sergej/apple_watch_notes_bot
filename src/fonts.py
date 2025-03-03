4ac8b4583b82fb49f037e76b316f4f7b4b9ce761
andreev-sergej <andreevsergej1111@gmail.com>
2025-03-04 01:33:35 +0300

    add fonts.py

---
+++ src/fonts.py
@@ -0,0 +1,17 @@
+BODY_FONTS = {
+    'Arial': 'Arial, sans-serif',
+    'Roboto': "'Roboto', sans-serif",
+    'Open Sans': "'Open Sans', sans-serif"
+}
+
+HEADER_FONTS = {
+    'Georgia': 'Georgia, serif',
+    'Roboto Slab': "'Roboto Slab', serif",
+    'Merriweather': "'Merriweather', serif"
+}
+
+CODE_FONTS = {
+    'Courier New': "'Courier New', monospace",
+    'Fira Code': "'Fira Code', monospace",
+    'Source Code Pro': "'Source Code Pro', monospace"
+}
